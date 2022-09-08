753b648b4e85e7e2ddc608c09399847a663cb188
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-09-08 12:54:38 -0300

    subo la pagina de pedidos, su ruta creada con los componentes de productos por pedir, carrito de pedidos y botón modal para enviar pedido

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Components/Header/Header';
 import LandingPage from './pages/LandingPage';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/Login';
+import PedidosPage from './pages/PedidosPage';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path='/'element= {<LandingPage/>}/>
         <Route path='/home'element= {<HomePage/>}/>
         <Route path='/login'element= {<LoginPage/>}/>
+        <Route path='/pedidos'element= {<PedidosPage/>}/>
       </Routes>
     </Router>
   );
