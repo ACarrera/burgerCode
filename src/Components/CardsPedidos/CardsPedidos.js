63a0073cf33843f8ddb68aef5f89d7ab5b093c96
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-09-09 00:12:52 -0300

    di estilo a las cards de productos

--- src/Components/CardsPedidos/CardsPedidos.js
+++ src/Components/CardsPedidos/CardsPedidos.js
@@ -3,69 +3,53 @@ import './CardsPedidos.css';
 
 const CardsPedidos = () => {
   return (
-    <div className="d-flex flex-wrap col">
-      <div className="card col-3">
-       <img src="https://picsum.photos/200/300?random=1"ssName className="card-img-top" alt="..." />
+    <div className="d-flex justify-content-evenly flex-wrap col">
+      <div className="card col-3 rounded-0 m-1">
+       <img src="https://picsum.photos/200/300?random=1" className="card-img-top border-0 rounded-0 border-0 rounded-0" alt="..." />
        <div className="card-body">
         <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         <a href="/" className="btn btn-primary">Go somewhere</a>
+        <p className="card-text">Descripción del producto.</p>
+         <a href="/" className="btn btn-warning rounded-0 rounded-0">Agregar al pedido</a>
        </div>
       </div>
-      <div className="card col-3">
-       <img src="https://picsum.photos/200/300?random=2"ssName className="card-img-top" alt="..." />
+      <div className="card col-3 rounded-0 m-1">
+       <img src="https://picsum.photos/200/300?random=2" className="card-img-top border-0 rounded-0 " alt="..." />
        <div className="card-body">
         <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         <a href="/" className="btn btn-primary">Go somewhere</a>
+        <p className="card-text">Descripción del producto.</p>
+         <a href="/" className="btn btn-warning rounded-0 rounded-0">Agregar al pedido</a>
        </div>
       </div>
-      <div className="card col-3">
-       <img src="https://picsum.photos/200/300?random=3"ssName className="card-img-top" alt="..." />
+      <div className="card col-3 rounded-0 m-1">
+       <img src="https://picsum.photos/200/300?random=3" className="card-img-top border-0 rounded-0 " alt="..." />
        <div className="card-body">
         <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         <a href="/" className="btn btn-primary">Go somewhere</a>
+        <p className="card-text">Descripción del producto.</p>
+         <a href="/" className="btn btn-warning rounded-0 rounded-0">Agregar al pedido</a>
        </div>
       </div>
-      <div className="card col-3">
-       <img src="https://picsum.photos/200/300?random=4"ssName className="card-img-top" alt="..." />
+      <div className="card col-3 rounded-0 m-1">
+       <img src="https://picsum.photos/200/300?random=4" className="card-img-top border-0 rounded-0 " alt="..." />
        <div className="card-body">
         <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         <a href="/" className="btn btn-primary">Go somewhere</a>
+        <p className="card-text">Descripción del producto.</p>
+         <a href="/" className="btn btn-warning rounded-0 rounded-0">Agregar al pedido</a>
        </div>
       </div>
-      <div className="card col-3">
-       <img src="https://picsum.photos/200/300?random=5"ssName className="card-img-top" alt="..." />
+      <div className="card col-3 rounded-0 m-1">
+       <img src="https://picsum.photos/200/300?random=5" className="card-img-top border-0 rounded-0 " alt="..." />
        <div className="card-body">
         <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         <a href="/" className="btn btn-primary">Go somewhere</a>
+        <p className="card-text">Descripción del producto.</p>
+         <a href="/" className="btn btn-warning rounded-0 rounded-0">Agregar al pedido</a>
        </div>
       </div>
-      <div className="card col-3">
-       <img src="https://picsum.photos/200/300?random=6"ssName className="card-img-top" alt="..." />
+      <div className="card col-3 rounded-0 m-1">
+       <img src="https://picsum.photos/200/300?random=6" className="card-img-top border-0 rounded-0 " alt="..." />
        <div className="card-body">
         <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         <a href="/" className="btn btn-primary">Go somewhere</a>
-       </div>
-      </div>
-      <div className="card col-3">
-       <img src="https://picsum.photos/200/300?random=7"ssName className="card-img-top" alt="..." />
-       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         <a href="/" className="btn btn-primary">Go somewhere</a>
-       </div>
-      </div>
-      <div className="card col-3">
-       <img src="https://picsum.photos/200/300?random=8"ssName className="card-img-top" alt="..." />
-       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         <a href="/" className="btn btn-primary">Go somewhere</a>
+        <p className="card-text">Descripción del producto.</p>
+         <a href="/" className="btn btn-warning rounded-0 rounded-0">Agregar al pedido</a>
        </div>
       </div>
     </div>
--- src/Components/CarritoPedidos/CarritoPedidos.js
+++ src/Components/CarritoPedidos/CarritoPedidos.js
@@ -4,12 +4,12 @@ import BotonEnviarPedido from '../BotonEnviarPedido/BotonEnviarPedido';
 const CarritoPedidos = () => {
   return (
       <div className="d-wrap w-25">
-        <ul class="list-group list-group-flush">
-         <li class="list-group-item">An item</li>
-         <li class="list-group-item">A second item</li>
-         <li class="list-group-item">A third item</li>
-         <li class="list-group-item">A fourth item</li>
-         <li class="list-group-item">And a fifth one</li>
+        <ul class="list-group list-group-flush d-flex">
+         <li class="list-group-item">Producto</li>
+         <li class="list-group-item">Producto</li>
+         <li class="list-group-item">Producto</li>
+         <li class="list-group-item">Producto</li>
+         <li class="list-group-item">Producto</li>
         </ul>
         <BotonEnviarPedido />
       </div>
