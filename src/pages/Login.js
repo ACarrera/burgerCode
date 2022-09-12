d71ffabc217c77137aeaa4f7f8f7786f01a346dc
Nahuel de la Vega <nahueldlvega@gmail.com>
2022-09-12 19:35:57 -0300

    cambios en login page, form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,34 @@
 // import Header from "../Components/Header/Header";
+import { Form, Button} from "react-bootstrap";
+import './Login.css'
 
-const LoginPage = () => {
-    return ( 
-        <>
-            <h1>Este es el Login</h1>
-        </>
-     );
+
+
+function LoginForm() {
+  return (
+    <Form className="container w-50 pt-5">
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Ingresa tu email" />
+        <Form.Text className="text-muted">
+          No compartiremos tu información con nadie.
+        </Form.Text>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Contraseña</Form.Label>
+        <Form.Control type="password" placeholder="Ingresa tu contraseña" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+      </Form.Group>
+      <Button variant="primary" type="submit" className="p-1 btn btn-dark">
+        Iniciar Sesión
+      </Button>
+      <Button variant="primary" type="submit" className="ms-2 p-1 btn btn-dark">
+      Registrarse  
+      </Button>
+    </Form>
+  );
 }
- 
-export default LoginPage;
+
+
+export default LoginForm
