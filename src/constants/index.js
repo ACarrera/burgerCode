a28a0077f656f27b1c777e24b55b948f36ec076c
Angel <ancar1286@hotmail.com>
2022-10-09 21:55:19 -0300

    mejoras en la funcionalidad

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,11 @@
-export const LOGIN_INITIAL_VALUES={
-    email:'',
-    password:''
-  };
+export const LOGIN_INITIAL_VALUES = {
+  email: "",
+  password: "",
+};
+export const ADD_USER_VALUES = {
+  name: "",
+  email: "",
+  password: "",
+  address: "",
+  phone: "",
+};
