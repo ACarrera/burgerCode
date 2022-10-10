8623b0fe6c9cbc75471c4b2ac9601cd0ebd0fbe0
Nahuel de la Vega <nahueldlvega@gmail.com>
2022-10-10 18:26:48 -0300

    cambios en header y login img

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -6,8 +6,8 @@ const LandingPage = () => (
         <div className="app-wrapper-info">
             <h3 className="h3-landing card-text">la mejor <FaHamburger className="h3-icon"/></h3>
             <h1 className="app-landing-h1 card-text h1-landing">burger</h1>
-            <h4 className="card-text h4-landing" style={{margin: '2rem 0'}}>Ingredientes Especiales</h4>
-            <p className="card-text p-landing">Carne de calidad, verduras frescas y pan casero recien salidos del horno.</p>
+            <h4 className="card-text h4-landing" style={{margin: '2rem 0'}}>INGREDIENTES ESPECIALES</h4>
+            <p className="card-text p-landing">Carne de calidad, verduras frescas y pan casero recién salidos del horno.</p>
             <button type="button" className="custom-button card-text">Quiero mi Burger</button>
         </div>
         <div className='bordes-img'>
